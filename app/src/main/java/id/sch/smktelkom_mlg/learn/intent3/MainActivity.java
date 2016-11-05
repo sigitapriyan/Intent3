e84f932c7024217efdf76ca74c267f9c0a80b2ef
sigitapriyan <dengansigit@gmail.com>
2016-11-05 10:37:45 +0700

    Intent SMS

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -22,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
                         dialPhoneNumber("0341712500");
                     }
                 });
+
+        findViewById(R.id.imageViewSMS)
+                .setOnClickListener( new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        composeSmsMessage("Pesan dari SMK Telkom Malang");
+                    }
+                });
     }
 
     public void dialPhoneNumber(String phoneNumber)
@@ -29,6 +39,15 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_DIAL);
         intent.setData(Uri.parse("tel:"+ phoneNumber));
         if (intent.resolveActivity(getPackageManager())!= null)
+            startActivity(intent);
+    }
+
+    public void composeSmsMessage(String message)
+    {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra("sms_body", message);
+        if (intent.resolveActivity(getPackageManager()) != null)
             startActivity(intent);
     }
 }
